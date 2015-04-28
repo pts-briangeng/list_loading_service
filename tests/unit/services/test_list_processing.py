723c2f7890425ff50ecb8b3ebc7089f4133b0f19
Harry Kim <harry.kim@points.com>
2015-04-28 14:28:04 -0400

    DPPTSOFFA-827 code review changes - add excel file format

--- tests/unit/services/test_list_processing.py
+++ tests/unit/services/test_list_processing.py
@@ -8,14 +8,24 @@ from app.services import elasticsearch_service
 import configuration
 import os
 import csv
+import copy
+import openpyxl
 
 
 class CsvMock(list):
     line_num = 1L
 
 
-class TestListProcessing(unittest.TestCase):
+class MockCell(object):
+    def __init__(self, cell_value):
+        self.cell_value = cell_value
 
+    @property
+    def value(self):
+        return self.cell_value
+
+
+class TestListProcessing(unittest.TestCase):
     def setUp(self):
         self.data = {
             'url': 'url',
@@ -32,9 +42,8 @@ class TestListProcessing(unittest.TestCase):
     @mock.patch.object(csv, 'reader', autospec=True)
     @mock.patch.object(elasticsearch_service, 'open', create=True)
     @mock.patch.object(os.path, 'isfile')
-    def test_elastic_search_operation(self, mock_is_file, mock_open, mock_csv_reader, mock_elastic_search,
-                                      mock_config, mock_bulk,
-                                      mock_cross_service_post):
+    def test_elastic_search_operation_with_csv(self, mock_is_file, mock_open, mock_csv_reader, mock_elastic_search,
+                                               mock_config, mock_bulk, mock_cross_service_post):
         mock_open.return_value = mock.MagicMock(spec=file)
         mock_csv_reader.return_value = CsvMock([['abc']])
 
@@ -44,7 +53,33 @@ class TestListProcessing(unittest.TestCase):
         elasticsearch_service.create_list(request)
 
         mock_bulk.assert_called_with(mock_elastic_search.return_value,
-                                     [{'_type': 'type', '_id': 1L, '_source': {'id': 'abc'}, '_index': 'index'}])
+                                     [{'_type': 'type', '_id': 'abc', '_source': {'accountNumber': 'abc'},
+                                       '_index': 'index'}])
+        mock_elastic_search.return_value.indices.refresh.assert_called_with(index='index')
+        mock_cross_service_post.assert_has_calls([mock.call(path='callback',
+                                                            data={'links': {'self': {'href': 'url'}}, 'success': True},
+                                                            service='index')])
+
+    @mock.patch.object(liblcp.cross_service, 'post_or_abort', autospec=True)
+    @mock.patch.object(helpers, 'bulk')
+    @mock.patch.object(configuration, 'data')
+    @mock.patch.object(elasticsearch, 'Elasticsearch')
+    @mock.patch.object(openpyxl, 'load_workbook', autospec=True)
+    @mock.patch.object(os.path, 'isfile')
+    def test_elastic_search_operation_excel(self, mock_is_file, mock_load_workbook, mock_elastic_search, mock_config,
+                                            mock_bulk, mock_cross_service_post):
+        mock_load_workbook.return_value.active.rows = [[MockCell('abc')]]
+
+        mock_elastic_search.return_value = mock.MagicMock()
+        mock_cross_service_post.return_value = True
+        data = copy.deepcopy(self.data)
+        data['file'] = 'file.xlsx'
+        request = models.Request(**data)
+        elasticsearch_service.create_list(request)
+
+        mock_bulk.assert_called_with(mock_elastic_search.return_value,
+                                     [{'_type': 'type', '_id': 'abc', '_source': {'accountNumber': 'abc'},
+                                       '_index': 'index'}])
         mock_elastic_search.return_value.indices.refresh.assert_called_with(index='index')
         mock_cross_service_post.assert_has_calls([mock.call(path='callback',
                                                             data={'links': {'self': {'href': 'url'}}, 'success': True},

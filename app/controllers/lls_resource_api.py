b88d3cff6c200199d4b94fc67dd437d89e731819
pts-ferdinandcardoso <ferdinand.cardoso@points.com>
2015-04-29 11:46:32 -0400

    DPPTSOFFA-827 : Back-end support for: As Simone, I can create a list for an offer. (API) -Update parameters to the request model

--- app/controllers/lls_resource_api.py
+++ app/controllers/lls_resource_api.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class CreateListPostResourceController(base.ResourceControllerMixin, controllers.PostResourceController):
+
     def __init__(self):
         super(CreateListPostResourceController, self).__init__(schema=post_list.REQUEST)
         self.http_successful_response_status = httplib.ACCEPTED
@@ -22,15 +23,8 @@ class CreateListPostResourceController(base.ResourceControllerMixin, controllers
         return CreateListPostResourceController
 
     def process_request_model(self, request_model, **kwargs):
-        url = self.request_url
-        index = kwargs.get("index", "")
-        type = kwargs.get("type", "")
-        callback_url = request_model.get("callbackUrl", "")
-        file = request_model.get("file", "")
-        request = models.Request(url=url, index=index, type=type, file=file, callbackUrl=callback_url)
-
+        request = models.Request(url=self.request_url, **dict(request_model, **kwargs))
         multiprocessing.Process(target=services.ListProcessing().create_list, args=(request,)).start()
-
         return {}
 
 
@@ -41,8 +35,5 @@ class ListStatusGetResourceController(base.ResourceControllerMixin, controllers.
         return ListStatusGetResourceController
 
     def get_resource_model(self, resource):
-        url = self.request_url
-        index = resource.get("index", "")
-        type = resource.get("type", "")
-        request = models.Request(url=url, index=index, type=type)
+        request = models.Request(url=self.request_url, **resource)
         return services.ListProcessing().get_list_status(request)

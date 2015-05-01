0f8e82e295460dade1ecbd7119488ef6e3fdc61a
Dave Weber <david.weber@points.com>
2015-05-01 15:42:05 -0400

    DPPTSOFFA-952: Delete list system integration tests

--- tests/integration/system/test_list_loading_service.py
+++ tests/integration/system/test_list_loading_service.py
@@ -9,7 +9,7 @@ from tests.integration import base
 from tests.integration import testing_utilities
 
 BASE_SERVICE_URL = 'http://0.0.0.0:5000/'
-CREATE_LIST_URL = 'index/offers/type/edaa3541-7376-4eb3-8047-aaf78af900da'
+BASE_LIST_URL = 'index/offers/type/edaa3541-7376-4eb3-8047-aaf78af900da'
 LIST_STATUS_URL = 'index/offers/type/edaa3541-7376-4eb3-8047-aaf78af900da/status'
 
 
@@ -22,15 +22,33 @@ class ListLoadingServiceIntegrationTest(base.BaseFullIntegrationTestCase):
 
     def test_create_list(self):
 
-        response = requests.post(BASE_SERVICE_URL + CREATE_LIST_URL, json.dumps(self.data), headers=self.headers)
+        # Create a list
+        response = requests.post(BASE_SERVICE_URL + BASE_LIST_URL, json.dumps(self.data), headers=self.headers)
         response_content = json.loads(response.content)
 
         tools.assert_equal(httplib.ACCEPTED, response.status_code)
-        tools.assert_in(CREATE_LIST_URL, response_content['links']['self']['href'])
+        tools.assert_in(BASE_LIST_URL, response_content['links']['self']['href'])
 
+        # Search for that list
         response = requests.get(BASE_SERVICE_URL + LIST_STATUS_URL, headers=self.headers)
         response_content = json.loads(response.content)
 
         tools.assert_equal(httplib.OK, response.status_code)
         tools.assert_in(LIST_STATUS_URL, response_content['links']['self']['href'])
         tools.assert_equal(5, response_content['hits']['total'])
+
+        # Delete the list
+        response = requests.delete(BASE_SERVICE_URL + BASE_LIST_URL, data=json.dumps({}), headers=self.headers)
+        response_content = json.loads(response.content)
+
+        tools.assert_equal(httplib.ACCEPTED, response.status_code)
+        tools.assert_in(BASE_LIST_URL, response_content['links']['self']['href'])
+        tools.assert_true(response_content['acknowledged'])
+
+        # Ensure the list was deleted
+        response = requests.get(BASE_SERVICE_URL + LIST_STATUS_URL, headers=self.headers)
+        response_content = json.loads(response.content)
+
+        tools.assert_equal(httplib.OK, response.status_code)
+        tools.assert_in(LIST_STATUS_URL, response_content['links']['self']['href'])
+        tools.assert_equal(0, response_content['hits']['total'])

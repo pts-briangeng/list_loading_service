2ae79f07225d09161a51fb4f57a284b37da59336
Harry Kim <harry.kim@points.com>
2015-05-05 14:00:41 -0400

    DPPTSOFFA-827 fix system integration test

--- tests/integration/system/test_list_loading_service.py
+++ tests/integration/system/test_list_loading_service.py
@@ -10,7 +10,7 @@ from tests.integration import base
 from tests.integration import testing_utilities
 
 BASE_SERVICE_URL = 'http://0.0.0.0:5000/'
-BASE_LIST_URL = 'index/offers/type/edaa3541-7376-4eb3-8047-aaf78af900da'
+BASE_LIST_URL = 'lists/offers/edaa3541-7376-4eb3-8047-aaf78af900da'
 LIST_STATUS_URL = 'lists/offers/edaa3541-7376-4eb3-8047-aaf78af900da/statistics'
 
 

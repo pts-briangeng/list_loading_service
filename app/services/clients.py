a5a3e4c1b84f899948d9d38d8c32bd0493e8b629
pts-ferdinandcardoso <ferdinand.cardoso@points.com>
2016-07-11 12:32:15 -0400

    DBGTLCP-2090 - As Roz, I can an upload a 5 million user list within 50% less time than today -reduce timeout time by reducing the retries (max_retries)

--- app/services/clients.py
+++ app/services/clients.py
@@ -2,18 +2,36 @@ import logging
 import traceback
 
 import elasticsearch
-from elasticsearch import exceptions
-from elasticsearch.client import query_params
+from elasticsearch import exceptions, connection, connection_pool, serializer, transport, client
 
 import configuration
 
 logger = logging.getLogger(__name__)
 
 
+class Transport(elasticsearch.Transport):
+
+    def __init__(self, hosts, connection_class=connection.Urllib3HttpConnection,
+                 connection_pool_class=connection_pool.ConnectionPool, host_info_callback=transport.get_host_info,
+                 sniff_on_start=False, sniffer_timeout=None, sniff_timeout=.1,
+                 sniff_on_connection_fail=False, serializer=serializer.JSONSerializer(), serializers=None,
+                 default_mimetype='application/json', max_retries=1, retry_on_status=(503, 504,),
+                 retry_on_timeout=False, send_get_body_as='GET', **kwargs):
+
+        super(Transport, self).__init__(
+            hosts, connection_class=connection_class, connection_pool_class=connection_pool_class,
+            host_info_callback=host_info_callback, sniff_on_start=sniff_on_start, sniffer_timeout=sniffer_timeout,
+            sniff_timeout=sniff_timeout, sniff_on_connection_fail=sniff_on_connection_fail, serializer=serializer,
+            serializers=serializers, default_mimetype=default_mimetype, max_retries=max_retries,
+            retry_on_status=retry_on_status, retry_on_timeout=retry_on_timeout,
+            send_get_body_as=send_get_body_as, **kwargs)
+
+
 class ElasticSearchClient(elasticsearch.Elasticsearch):
 
     def __init__(self, **kwargs):
-        super(ElasticSearchClient, self).__init__(hosts=[configuration.data.ELASTIC_SEARCH_SERVER], **kwargs)
+        super(ElasticSearchClient, self).__init__(
+            hosts=[configuration.data.ELASTIC_SEARCH_SERVER], transport_class=Transport, **kwargs)
 
     def _create_es_index_if_required(self, index):
         try:
@@ -43,7 +61,7 @@ class ElasticSearchClient(elasticsearch.Elasticsearch):
             logger.warning(traceback.format_exc(), exc_info=1)
             raise e
 
-    @query_params('consistency', 'refresh', 'routing', 'replication', 'timeout')
+    @client.query_params('consistency', 'refresh', 'routing', 'replication', 'timeout')
     def bulk(self, body, index=None, doc_type=None, params=None):
         self._create_es_index_if_required(index)
         self._create_es_mapping(index, doc_type)

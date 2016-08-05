6bf4b1f444332dda00fae0870892d9b91879daec
pts-briangeng <brian.geng@points.com>
2016-08-05 10:38:13 -0400

    DBGTLCP-28
    - cr fixes

--- app/controllers/schemas/append_list.py
+++ app/controllers/schemas/append_list.py
@@ -1,7 +1,7 @@
 REQUEST = {
     "id": "",
     "$schema": "http://json-schema.org/draft-04/schema#",
-    "description": "Schema a request for creating an offer list",
+    "description": "Schema a request for appending to an offer list",
     "type": "object",
     "required": ["filePath"],
     "properties": {

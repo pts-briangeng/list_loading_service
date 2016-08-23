f3912dff73c6cde88eb32e7cc345ddceab93c20b
pts-ferdinandcardoso <ferdinand.cardoso@points.com>
2016-08-23 10:15:04 -0400

    Backed out changeset: d34260d033bb

--- tests/integration/sanity/test_list_loading_service_sanity.py
+++ tests/integration/sanity/test_list_loading_service_sanity.py
@@ -62,10 +62,10 @@ def _get_testing_data():
         "2045ecfd-7f7c-4b04-ae27-f85af578d574",
         "2b37b80e-e89e-49d2-91b6-ccb90f59d2a4",
         "10afc5e5-18b1-4e42-b453-ca4d2e814ab0",
-        # "اختبار",
-        # "一二三二百",
-        # "ANGÈLE",
-        # "двадцать четыре"
+        "اختبار",
+        "一二三二百",
+        "ANGÈLE",
+        "двадцать четыре"
     ]
     return len(account_numbers), random.choice(account_numbers)
 
